3a2f441c4e1740f611e784292536a48931b2b0cb
MayraM5 <mayra.mercado@tuta.io>
2022-11-03 01:15:58 -0600

    Check for Faulty User Input

--- game.py
+++ game.py
@@ -2,24 +2,32 @@
 
 # Put your code here
 import random
-print("Welcome to guessing name")
+print("Welcome to guessing game!")
 name = (input("Hello, Whats your name? "))
 correct_num = random.randint(1, 5)
 wrong_guesses = 1
 
 while True:
-    chosen_num = int(input(f"{name}, please choose number between 1-5: "))
-    
-    if chosen_num != correct_num:
+    chosen_num = input(f"{name}, please choose number between 1-5: ")
+    try:
+        num = int(chosen_num)
+    except ValueError:
+        print("Error! Must be numbers")
+        continue
+
+    if num < 1 or num > 5:
+        print("Error! Try again")
         
-        if chosen_num < correct_num:
-            print(f'Your guess? {chosen_num}')
+    if num != correct_num:
+        
+        if num < correct_num:
+            print(f'Your guess? {num}')
             print ("Your guess is too low, try again!")
-        elif chosen_num > correct_num:
-            print(f'Your guess? {chosen_num}')
+        elif num > correct_num:
+            print(f'Your guess? {num}')
             print ("Your guess is too high, try again!")       
         wrong_guesses +=1    
-        
+            
     else:
         print (f"Well done {name}! You found my number in {wrong_guesses} tries!")
-        break 
+        break        
